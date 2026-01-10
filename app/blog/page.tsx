56238cc0f0a4f1f18bd7b5184f8bf3ef8f97c3e7
pando <pandora.va7@gmail.com>
2026-01-10 23:21:01 +0800

    sitemap

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,8 +7,8 @@ export const metadata: Metadata = {
   title: "潘多拉 | 博客",
   description: "潘多拉个人档案馆网站内的博客，分享个人见解和知识",
   openGraph: {
-    title: "潘多拉 | 博客 openGraph测试",
-    description: "openGraph测试的描述",
+    title: "潘多拉 | 博客",
+    description: "潘多拉个人档案馆网站内的博客，分享个人见解和知识",
     url: "https://pandorava7.com",
     images: [
       {
