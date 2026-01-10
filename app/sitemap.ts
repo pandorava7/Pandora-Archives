5427e84c6ec58a7739eae8882cf42f19f105893d
pando <pandora.va7@gmail.com>
2026-01-10 23:22:43 +0800

    sitemap update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 import path from 'path'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://pandorava7.com'
+  const baseUrl = 'https://www.pandorava7.com'
 
   // 1. 获取 posts.json 的绝对路径
   // process.cwd() 返回项目根目录
@@ -17,7 +17,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     const fileContent = fs.readFileSync(filePath, 'utf8')
     const posts = JSON.parse(fileContent)
     console.log('读取到的 posts:', posts) // 这会在你的终端（终端终端，不是浏览器）里显示
-    
+
     // 3. 遍历对象生成 /blog/{id} 路径
     // 假设你的 JSON 格式是 [{ "id": "post-1" }, { "id": "post-2" }]
     dynamicEntries = posts.map((post: { id: string }) => ({
